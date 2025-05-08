3c81d1e699283281e49670416dd62cea91ccf937
Charlton Lane <34801673+CharltonLane@users.noreply.github.com>
2025-05-08 13:02:56 +1000

    Remove Windows specific code for file io
    - Was using windows methods to get the program data folder, but that's overkill so I'm just saving the highscore locally to wherever the exe is.

--- saveData.h
+++ saveData.h
@@ -4,69 +4,56 @@
 #include <fstream>
 #include <iostream>
 
-#define NOMINMAX
-#include "ShlObj.h"
 
 namespace SaveData {
 
-	inline int g_highscore{0};
+	inline int g_highscore{ 0 };
 	inline const std::string g_highscoreFileName{ "highscore.txt" };
-	inline const std::string g_dataFolderName{ "WoolieInvaders" };
+	inline const std::string g_dataFolderName{ "SaveData" };
 
-	inline static int ReadHighscoreFromDisk() {
+	inline static void ReadHighscoreFromDisk() {
 		// Read saved high score.
-		TCHAR windowsProgramDataPath[MAX_PATH];
-		if (SUCCEEDED(SHGetFolderPath(NULL, CSIDL_COMMON_APPDATA, NULL, 0, windowsProgramDataPath)))
-		{
-			std::wstring test{ windowsProgramDataPath }; //convert to wstring
-			std::string test2{ test.begin(), test.end() }; //and convert to string.
 
-			// Now load our file if possible.
-			std::filesystem::path dataDirectoryPath{ test2 + "\\" + g_dataFolderName };
-			if (std::filesystem::is_directory(dataDirectoryPath)) {
-				// Check if file exists.
-				std::filesystem::path highscoreFilePath{ dataDirectoryPath.string() + std::string{"\\" + g_highscoreFileName} };
-				if (std::filesystem::exists(highscoreFilePath)) {
-					// Open the file and read the first line.
-					std::ifstream inf{ highscoreFilePath };
-					std::string highScoreData{};
-					std::getline(inf, highScoreData);
-					int score{ std::stoi(highScoreData) };
-					// TODO: Error handling!
-					g_highscore = score;
-				}
-				else {
-					std::cout << "No file: " << highscoreFilePath;
-				}
+		// Now load our file if possible.
+		std::filesystem::path dataDirectoryPath{ g_dataFolderName };
+		if (std::filesystem::is_directory(dataDirectoryPath)) {
+			// Check if file exists.
+			std::filesystem::path highscoreFilePath{ dataDirectoryPath.string() + std::string{"\\" + g_highscoreFileName} };
+			if (std::filesystem::exists(highscoreFilePath)) {
+				// Open the file and read the first line.
+				std::ifstream inf{ highscoreFilePath };
+				std::string highScoreData{};
+				std::getline(inf, highScoreData);
+				int score{ std::stoi(highScoreData) };
+				// TODO: Error handling!
+				std::cout << "Loaded highscore: " << score << " from " << highscoreFilePath << "\n";
+				g_highscore = score;
 			}
 			else {
-				std::cout << "No folder: " << dataDirectoryPath;
+				std::cout << "No file: " << highscoreFilePath << "\n";
 			}
-
 		}
-		return 0;
+		else {
+			std::cout << "No folder: " << dataDirectoryPath << "\n";
+		}
 	}
 
 	inline static void WriteHighscoreToDisk(int score) {
 		g_highscore = score;
 
-		TCHAR windowsProgramDataPath[MAX_PATH];
-		if (!SUCCEEDED(SHGetFolderPath(NULL, CSIDL_COMMON_APPDATA, NULL, 0, windowsProgramDataPath)))
-		{
-			return;
-		}
-
-		std::wstring test{ windowsProgramDataPath }; //convert to wstring
-		std::string test2{ test.begin(), test.end() }; //and convert to string.
 
 		// Woolie invaders directory.
-		std::filesystem::path dataDirectoryPath{ test2 + "\\" + g_dataFolderName };
+		std::filesystem::path dataDirectoryPath{ g_dataFolderName };
 		if (!std::filesystem::is_directory(dataDirectoryPath)) {
 			// Create directory.
 			bool success = std::filesystem::create_directory(dataDirectoryPath);
+			if (!success) {
+				std::cerr << "Failed to create directory: " << dataDirectoryPath << " Not saving highscore!\n";
+				return;
+			}
 		}
 
-		std::filesystem::path highscoreFilePath{ dataDirectoryPath.string() + std::string{"\\" + g_highscoreFileName}};
+		std::filesystem::path highscoreFilePath{ dataDirectoryPath.string() + std::string{"\\" + g_highscoreFileName} };
 
 		// Open (create and open if file doesn't exist) the file for writing.
 		std::ofstream outFile{ highscoreFilePath };
@@ -80,7 +67,7 @@ namespace SaveData {
 
 		// Write the new highscore to the file.
 		outFile << score;
-
+		std::cout << "Highscore of: " << score << " written to " << highscoreFilePath << "\n";
 		// File closes once outf is out of scope.
 	}
 }
